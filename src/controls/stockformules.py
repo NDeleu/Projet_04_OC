232aeb690d3fdaf796a5a07efddb608d83987733
Nicolas Deleu <n.deleu@hotmail.fr>
2022-12-27 21:12:19 +0100

    Actualisation des notes de test

--- src/controls/stockformules.py
+++ src/controls/stockformules.py
@@ -1,20 +1,7 @@
 
 
 # Tournoi :
-
-"""
-Créer la même pour self.date et self.round
-
-indice pr round : Round(self.liste_joueur, self.tours_round)
-
-Pour la date, aller chercher les dates de round, mais en changeant le format, soit :
-print(datetime.datetime.strptime(round1.start_time, "%Y-%m-%d %H:%M:%S").strftime("%Y-%m-%d"))
-pour cela on va repasser la date de str à objet, puis de objet en str en lui demandant de ne prendre que année mois jour
-puis on fait un self.date.append(la date qu'on a créé) avec condition qu'il n'existe pas, donc :
-if not datecréé in self.date :
-    self.date.append(datecréé) 
-"""
-
+# view :
 """
 Créer ds le view une condition au contrôle du tps : soit :
     Bullet
